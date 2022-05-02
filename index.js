603afb06e34c3aa7d39413b88132d699c1a94f3b
Rahad Khan <clash146098@gmail.com>
2022-05-02 14:58:00 +0600

    all cars and single cars api created

--- index.js
+++ index.js
@@ -2,11 +2,55 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 // Midware
 
 app.use(cors());
 app.use(express.json());
+//
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@auto-cars-cluster.qvigm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+    try {
+        await client.connect();
+
+        const carsCollection = client.db("carsDB").collection("products");
+        // Load All Cars
+        app.get('/cars', async (req, res) => {
+            const query = {};
+            const cursor = carsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+        //Load single cars
+        app.get('/cars/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await carsCollection.findOne(query);
+            res.send(result)
+            console.log(id , result);
+        })
+
+    }
+    finally{
+        // console.log("object");
+    }
+}
+
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send("wearhouse is active")
